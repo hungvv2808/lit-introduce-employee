58505f1cf25d95b85656d0b1bd4c8cdcfe1d8a51
hungvv2808 <vuhung2881999@gmail.com>
2022-11-10 18:47:40 +0700

    update url images

--- js/const.js
+++ js/const.js
@@ -2,11 +2,11 @@ let isActive = false;
 const defaultData = {
   title: "Thông báo nhân sự mới",
   content: "Xin chào tất cả thành viên LitCommerce,<br><br>Phòng Nhân sự xin trân trọng giới thiệu <b class='txt-blue'>Anh Đào Thế Hải</b>, chính thức gia nhập LitCommerce với vị trí <b class='txt-blue'>Account Manager</b> kể từ ngày hôm nay.<br><br>Chúng ta cùng chào đón anh Hải đến với đại gia đình LitCommerce và chúc anh Hải hoàn thành tốt các công việc sắp tới và gắn bó với công ty thật dài lâu.<br><br><b>PHÒNG NHÂN SỰ</b>",
-  avatar: "/images/avt.jpg",
+  avatar: "./images/avt.jpg",
   name: "Đào Thế Hải (Leo)",
   dateOfBirth: "1991",
   position: "Account Manager",
-  backgroundImage: "/images/welcome.jpg",
+  backgroundImage: "./images/welcome.jpg",
 };
 
 $('#summernote').summernote({
